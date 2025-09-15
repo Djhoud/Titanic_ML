94ba42f2bfa8b89a04aaf67ba023599392ff7330
Djhoud <luizoxvaldo@gmail.com>
2025-09-15 18:41:28 -0300

    Atualizalizações

--- titanic1.py
+++ titanic1.py
@@ -1,25 +1,48 @@
 import pandas as pd
-import numpy as np 
+from sklearn.ensemble import RandomForestClassifier
+
 
 # Carregando os dados
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
 
-# Exibindo as primeiras linhas (opcional)
-print(train.head()) 
-
-# Criando a função de transformação de sexo
+# Função para transformar sexo em binário
 def transformar_sexo(valor):
-    if valor == 'female':
+    if valor == "female":
         return 1
     else:
         return 0
 
-# Criando a nova coluna 'sex_binario' usando a função
-train['sex_binario'] = train['Sex'].map(transformar_sexo)
+# Criando a nova coluna no train
+train["Sex_binario"] = train["Sex"].map(transformar_sexo)
 
-# Preparando o modelo (note que a variável 'sex_binario' agora existe)
-from sklearn.ensemble import RandomForestClassifier
+# Selecionando variáveis
+variaveis = ["Sex_binario", "Age"]
 
+# Tratando valores ausentes em Age
+train["Age"] = train["Age"].fillna(-1)
+test["Age"] = test["Age"].fillna(-1)
+
+# Preparando X e y
+x = train[variaveis]
+y = train["Survived"]
+
+# Criando o modelo
 modelo = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=0)
-variaveis = ['sex_binario', 'age'] # 'age' precisa ser tratada se tiver valores ausentes
+modelo.fit(x, y)
+
+# Criando a coluna no test também
+test["Sex_binario"] = test["Sex"].map(transformar_sexo)
+x_prev = test[variaveis]
+
+# Fazendo previsões
+p = modelo.predict(x_prev)
+
+# Criando o arquivo de submissão
+submissao = pd.DataFrame({
+    "PassengerId": test["PassengerId"],
+    "Survived": p
+})
+
+submissao.to_csv("submission.csv", index=False)
+print("Arquivo submission.csv criado com sucesso!")
